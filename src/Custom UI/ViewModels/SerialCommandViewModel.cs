220b601af12c33f226990dce146ac769a4be11fb
patrickhui2013047 <57173232+patrickhui2013047@users.noreply.github.com>
2021-01-27 21:54:00 +0800

    testing state 1

---
+++ src/Custom UI/ViewModels/SerialCommandViewModel.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using Caliburn.Micro;
+using Custom_UI.Framework;
+using Custom_UI.Messages;
+
+namespace Custom_UI.ViewModels
+{
+    public class SerialCommandViewModel : PropertyChangedBase, IHandle<SerialPortConnect>, IHandle<SerialPortDisconnect>, IHandle<ConnectionError>
+    {
+        private readonly IEventAggregator _eventAggregator;
+
+        private string _dataToSend = string.Empty;
+        public string DataToSend
+        {
+            get { return _dataToSend; }
+            set
+            {
+                _dataToSend = value;
+                NotifyOfPropertyChange(() => DataToSend);
+                NotifyOfPropertyChange(() => IsValidData);
+            }
+        }
+
+        private bool _isText = true;
+        public bool IsText
+        {
+            get { return _isText; }
+            set
+            {
+                _isText = value;
+                NotifyOfPropertyChange(() => IsText);
+                NotifyOfPropertyChange(() => IsValidData);
+            }
+        }
+
+        private bool _isHex = false;
+        public bool IsHex
+        {
+            get { return _isHex; }
+            set
+            {
+                _isHex = value;
+                NotifyOfPropertyChange(() => IsHex);
+                NotifyOfPropertyChange(() => IsValidData);
+            }
+        }
+
+        public bool IsValidData
+        {
+            get
+            {
+                if (!IsConnected)
+                {
+                    return false;
+                }
+
+                if (DataToSend.Length <= 0)
+                {
+                    return false;
+                }
+
+                if (IsText) // Left this here for readability even though it is not needed. Otherwise someone might think it was missing or forgotten about.
+                {
+                }
+
+                if (IsHex)
+                {
+                    try
+                    {
+                        string[] characters = DataToSend.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                        foreach (string hex in characters)
+                        {
+                            byte value = Convert.ToByte(hex, 16);
+                        }
+                    }
+                    catch
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        private bool _isConnected = false;
+        public bool IsConnected
+        {
+            get { return _isConnected; }
+            set
+            {
+                _isConnected = value;
+                NotifyOfPropertyChange(() => IsConnected);
+                NotifyOfPropertyChange(() => IsValidData);
+            }
+        }
+
+        public SerialCommandViewModel(IEventAggregator eventAggregator)
+        {
+            _eventAggregator = eventAggregator;
+            _eventAggregator.Subscribe(this);
+        }
+
+        public void DoSend()
+        {
+            List<byte> data = new List<byte>();
+
+            if (IsHex)
+            {
+                string[] characters = DataToSend.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (string hex in characters)
+                {
+                    byte value = Convert.ToByte(hex, 16);
+                    data.Add(value);
+                }
+            }
+
+            if (IsText)
+            {
+                string parsed = DataToSend.Replace("\\\\r", "\r").Replace("\\\\n", "\n");
+                data.AddRange(System.Text.Encoding.ASCII.GetBytes(parsed));
+            }
+
+            _eventAggregator.PublishOnUIThread(new SerialPortSend() { Data = data.ToArray() });
+        }
+
+
+        public void Add()
+        {
+
+        }
+
+
+        public void Handle(SerialPortConnect message)
+        {
+            IsConnected = true;
+        }
+
+        public void Handle(SerialPortDisconnect message)
+        {
+            IsConnected = false;
+        }
+
+        public void Handle(ConnectionError message)
+        {
+            IsConnected = false;
+        }
+    }
+}
---
+++ src/Custom UI/Views/SerialCommandView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Custom_UI.Views
+{
+    /// <summary>
+    /// SerialCommandView.xaml 的互動邏輯
+    /// </summary>
+    public partial class SerialCommandView : UserControl
+    {
+        public SerialCommandView()
+        {
+            InitializeComponent();
+            CommandView.Items.Add(new object());
+
+
+        }
+    }
+}
